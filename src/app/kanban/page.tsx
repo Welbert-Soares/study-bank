b0ace3274e4e9015a5c4656423acfe01cb2a2feb
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-05-15 02:42:36 -0300

    refactor: remove Kanban page and Notion service files

--- src/app/kanban/page.tsx
+++
@@ -1,9 +0,0 @@
-import KanbanBoard from '@/components/kanban-board'
-
-export default function KanbanPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <KanbanBoard />
-    </div>
-  )
-}
