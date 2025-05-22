284ec574e51fe66f4a96d72717860155a7167d5e
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-05-22 02:12:32 -0300

    fix: update allowedOrigins in Next.js config to include wildcard for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {
     serverActions: {
       allowedOrigins: [
         'localhost:3000',
-        'study-bank-6y85a0k72-welbert-soares-projects.vercel.app',
+        'study-bank-6y85a0k72-welbert-soares-projects.vercel.app/*',
+        'https://study-bank.vercel.app',
+        'https://study-bank.vercel.app/*',
       ],
     },
   },
